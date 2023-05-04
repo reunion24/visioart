650d7632f3620370efdefb0fe98a125a57534c05
reunion24 <ada24men@gmail.com>
2023-05-04 16:03:53 +0300

    test

--- app/page.tsx
+++ app/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
 			<div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 			<div className="my-16 text-center animate-fade-in">
 				<h2 className="text-sm text-zinc-500 ">
-					COMING SOON
+					COMING SOONa
 				</h2>//
 			</div>
 		</div>
