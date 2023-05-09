937bde5d54637863a9e6903502898d6254b36850
reunion24 <ada24men@gmail.com>
2023-05-09 17:48:15 +0300

    test_09_05-02-new4

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -2,27 +2,40 @@ import Link from "next/link";
 import React from "react";
 import Particles from "../components/particles";
 
-
-//class Home extends React.Component {
-//	static Props = {
-//	  name: 'World'
-//	}
-  
-//	render() {
-//	  return <h1>Hello, {this.props.name}</h1>;
-//	}
-//  }
-
-
-
 interface Props {
-  name: 'World'
+  name: string;
 }
 
-class Navigation extends React.Component<Props> {
+class Visioart extends React.Component<Props> {
+  static defaultProps = {
+    name: 'visioart.io'
+  }
+
   render() {
     return <h1>Hello, {this.props.name}</h1>
   }
 }
 
-export default Navigation;
+export default Visioart;
+
+//function visioart.io React.Component {
+	//static Props = {
+	//  name: 'visioart.io'
+	//}
+  
+//	render() {
+	//  return <h1>Hello, {this.props.name}</h1>
+//	}
+ // }
+
+
+
+//interface Props {
+  //name: 'World'
+//}
+
+//export default async function Home() {
+  
+  //  return <h1>Hello, {this.props.name}</h1>
+  
+//}
