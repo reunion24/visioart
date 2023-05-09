5c6ff0ad458bb66044602a1a996496a1dec13f92
reunion24 <ada24men@gmail.com>
2023-05-09 17:15:52 +0300

    test_09_05-02-new2

--- app/page copy.tsx
+++ app/page copy.tsx
@@ -3,12 +3,26 @@ import React from "react";
 import Particles from "./components/particles";
 
 
-class Hello extends React.Component {
-	static defaultProps = {
-	  name: 'World'
-	}
+//class Home extends React.Component {
+//	static Props = {
+//	  name: 'World'
+//	}
   
-	render() {
-	  return <h1>Hello, {this.props.name}</h1>;
-	}
+//	render() {
+//	  return <h1>Hello, {this.props.name}</h1>;
+//	}
+//  }
+
+
+
+interface Props {
+  name: 'World';
+}
+
+class Hello extends React.Component<Props> {
+  render() {
+    return <h1>Hello, {this.props.name}</h1>;
   }
+}
+
+export default Hello;
