65d20296ac1e72313df0d3e4f232b0d900eef80b
Leon Nikishaev <leonnikishaev@Leons-MacBook-Pro.local>
2023-05-13 03:22:31 +0300

    12-05-session

--- app/page.tsx
+++ app/page.tsx
@@ -18,11 +18,9 @@ const Home: React.FC = () => {
   return (
     <div>
       <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black relative">
-        <Navigation />
-          {/* <Link href="/about-us"><a><AboutUs /></a></Link>
-          <Link href="/contact"><a><ContactUs /></a></Link>
-          <Link href="/works"><a><Works /></a></Link>
-        </Navigation> */}
+          {/* <Navigation /> */}
+       
+
         <div className="absolute inset-0 -z-10">
           <Particles className="h-full w-full animate-fade-in" quantity={100} />
         </div>
@@ -30,8 +28,19 @@ const Home: React.FC = () => {
           <h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text">
             visioart.io
           </h1>
+          <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+          <div className="my-16 text-center animate-fade-in">
+            <h1 className="text-sm text-zinc-500 ">
+              COMING SOON
+            </h1>
+          </div>
+
         </div>
       </div>
+
+
+
+{/* 
       <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black relative">
         <div className="absolute inset-0 -z-10">
           <Particles className="h-full w-full animate-fade-in" quantity={100} />
@@ -58,11 +67,11 @@ const Home: React.FC = () => {
           <Particles className="h-full w-full animate-fade-in" quantity={100} />
         </div>
         <div className="my-16 text-center animate-fade-in">
-          <div className=" mx-auto text-white">
+          <div className="text-white">
             <Contact />
           </div>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
