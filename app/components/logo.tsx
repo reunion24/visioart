036a6bf24ebf269473c505c210046790f2f83fc1
אליזה <eliza@h-MacBook-Pro-sl-lyzh.local>
2023-06-03 18:51:28 +0300

    Added scroll param

--- app/components/logo.tsx
+++ app/components/logo.tsx
@@ -5,7 +5,7 @@ import { handleSlide } from "./nav";
 
 
 export default function LogoComponent(props: { scrolled: any; }) {
-    const [style, setStyle] = useState({ opacity: 0 });
+    const [scroll, setScroll] = useState(1);
 
     useEffect(() => {
       const handleScroll = () => {
@@ -13,13 +13,13 @@ export default function LogoComponent(props: { scrolled: any; }) {
         const fastScroll = scrolled * 6;
      
 
-        let newOpacity = 0 + fastScroll;
-        if (newOpacity < 0) newOpacity = 0;
-        if (newOpacity > 1) newOpacity = 1;
+        let newScroll = 0 + fastScroll;
+        if (newScroll < 0) newScroll = 0;
+        if (newScroll > 1) newScroll = 1;
   
        
   
-        setStyle({ opacity: newOpacity });
+        setScroll(1-newScroll);
       };
   
       window.addEventListener("scroll", handleScroll);
@@ -29,11 +29,11 @@ export default function LogoComponent(props: { scrolled: any; }) {
     }, []);
    var classes = "z-999 text-4xl text-transparent duration-1000 justify-end cursor-default text-edge-outline animate-title font-display sm:text-4xl md:text-6xl whitespace-nowrap bg-clip-text"
   return (
-    <div id="logo_nav" className="flex flex-col items-center justify-center">
+    <div id="logo-nav" style={{'top':  scroll * 43 + 'vh', 'scale': scroll * 1.1 + 1}} className="flex flex-col items-center justify-center">
       <div className="flex flex-col items-center justify-center h-full">
         <div className=" inset-0 -z-10"></div>
-        <div style={style} >
-          <h1 className={ props.scrolled ? classes + " visio-bg": classes }>
+        <div>
+          <h1 className={ props.scrolled ? classes + " visio-bg": classes + " bg-white" }>
           <button className="duration-200 cursor-pointer" onClick={() => handleSlide('header')}>visioart.io</button>
           </h1>
         </div>
