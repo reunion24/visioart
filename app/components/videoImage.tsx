6d6d6a9c06400c907f439afa7be7eb8b95964f01
Leon Nikishaev <leonnikishaev@Leons-MBP.station>
2023-11-08 02:06:24 +0100

    Updated Works.css and new File

--- app/components/videoImage.tsx
+++ app/components/videoImage.tsx
@@ -17,12 +17,12 @@ const imageFilenames = [
 ];
 
 const imageNames = [
-    'Stereotype & jacob.\nBACK AGAIN',
-    '1748980023-7f878edef28157bf0b75588bbb6f53b423ea79d20cef6e65c2db0e9f72fb2302-d?mw=1600&mh=900&q=70', 
-    '1748969030-97a94d85a4a08f7b0a6eafa06c65f5bafc0e1d7ba4ce445eaa8b3f2f414d3a7f-d?mw=1600&mh=900&q=70', 
-    '1724118929-da4c387b9893d3aff49471b060ed42a1d9b3ee200848d6275fd32dfbf372e4de-d?mw=1200&mh=900&q=70', 
-    '1748970833-746b5ce2e4544187652b0681b5f40ded3be3f224947732963e41fa89323faed8-d?mw=1600&mh=900&q=70', 
-    '1748971656-c560593821160132d29215a747e07db4dfc5ec489b4c2303b2051561e10d7d96-d?mw=1700&mh=896&q=70'
+    'STEREOTYPE & jacob.\nBACK AGAIN',
+    '...backagain.backagain...', 
+    '[NOTHING004]', 
+    'STEREOTYPE\n"LONDON"', 
+    'TVOROZHNOE OZERO\nDREAM', 
+    '"AU-THEN-TIC"'
 ];
 
 function VideoImage(props: VideoImageProps) {
@@ -47,6 +47,7 @@ function VideoImage(props: VideoImageProps) {
                 >
                     <div className="video-overlay">
                         <span className='video-link' style={{
+                           // fontWeight: 'bold',
                             whiteSpace:"pre-line"
                             }}
                             >
