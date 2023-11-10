fdf275947b1d8a0ab38770a24766f021e7dccefb
Leon Nikishaev <leonnikishaev@Leons-MBP.lan>
2023-11-10 01:37:27 +0100

    Media Query for Works.css 2

---
+++ app/test/page.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Link from 'next/link';
+import { AlignJustify } from 'lucide-react';
+import './style.css';
+
+
+const Home = () => {
+
+    return(
+
+<div className="grid-container">
+  <div className="grid-item">1</div>
+  <div className="grid-item">2</div>
+  <div className="grid-item">3</div>
+  <div className="grid-item">4</div>
+  <div className="grid-item">5</div>
+  <div className="grid-item">6</div>
+  <div className="grid-item">7</div>
+  <div className="grid-item">8</div>
+  <div className="grid-item">9</div>
+  </div>
+    );
+};
+  
+
+export default Home;
