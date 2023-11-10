81307f5e480f8e26afdd5e990ab167b6a8d01d63
Leon Nikishaev <leonnikishaev@Leons-MBP.lan>
2023-11-10 03:16:34 +0100

    New Slider with the Media Query in test/page.tsx

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,14 +1,62 @@
-import React from 'react';
+"use client";
+import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { AlignJustify } from 'lucide-react';
 import './style.css';
 
 
+const items = Array.from({ length: 12 }, (_, i) => `Item ${i + 1}`);
+
 const Home = () => {
+  // Initialize windowWidth as undefined and declare it can be a number or undefined
+  const [windowWidth, setWindowWidth] = useState<number | undefined>(undefined);
 
-    return(
+  useEffect(() => {
+    // Set the initial value for windowWidth if window is defined
+    if (typeof window !== 'undefined') {
+      setWindowWidth(window.innerWidth);
+    }
 
-<div className="grid-container">
+    // Handle window resize
+    function handleResize() {
+      setWindowWidth(window.innerWidth);
+    }
+
+    // Add event listener for window resize
+    window.addEventListener('resize', handleResize);
+
+    // Clean up event listener
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  // Determine the number of items per slide based on window width
+  // Default to 4 items per slide if windowWidth is not set
+  const itemsPerSlide = windowWidth ? (windowWidth > 600 ? 4 : 3) : 4;
+
+  // Divide items into slides
+  const slides = [];
+  for (let i = 0; i < items.length; i += itemsPerSlide) {
+    slides.push(items.slice(i, i + itemsPerSlide));
+  }
+
+  return (
+    <div className="holster">
+      <div className="visio-container">
+        {slides.map((slideItems, index) => (
+          <div key={index} className="slide">
+            {slideItems.map((item, itemIndex) => (
+              <div key={itemIndex} className="item">
+                {item}
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+    </div>
+
+  
+
+/* <div className="grid-container">
   <div className="grid-item">1</div>
   <div className="grid-item">2</div>
   <div className="grid-item">3</div>
@@ -18,7 +66,7 @@ const Home = () => {
   <div className="grid-item">7</div>
   <div className="grid-item">8</div>
   <div className="grid-item">9</div>
-  </div>
+  </div> */
     );
 };
   
