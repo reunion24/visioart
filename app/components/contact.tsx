7609f9d03868c4c9860466ed383607af564eebc7
Adam <ada24men@gmail.com>
2023-11-20 22:57:18 +0100

    vimeo icon

--- app/components/contact.tsx
+++ app/components/contact.tsx
@@ -3,7 +3,7 @@ import { Instagram, Tv, Mail } from "lucide-react";
 import Link from "next/link";
 import { Card } from "./card";
 import '../style.css';
-// import { ReactComponent as VimeoIcon } from '/public/vimeo-icon.svg';
+import { color } from "framer-motion";
 
 const socials = [
 	{
@@ -19,8 +19,8 @@ const socials = [
 		handle: "adam@visioart.io",
 	},
 	{
-		icon: <Tv size={20} />,
-		href: "https://vimeo.com/visioart",
+		icon: <svg xmlns="http://www.w3.org/2000/svg"  width="20" height="20" viewBox="0 0 24 24"><path d="M22.875 10.063c-2.442 5.217-8.337 12.319-12.063 12.319-3.672 0-4.203-7.831-6.208-13.043-.987-2.565-1.624-1.976-3.474-.681l-1.128-1.455c2.698-2.372 5.398-5.127 7.057-5.28 1.868-.179 3.018 1.098 3.448 3.832.568 3.593 1.362 9.17 2.748 9.17 1.08 0 3.741-4.424 3.878-6.006.243-2.316-1.703-2.386-3.392-1.663 2.673-8.754 13.793-7.142 9.134 2.807z" fill="white"/></svg>,
+		href: "https://www.youtube.com/channel/UCYPh0TPIcljdsNYJiMMWtPA",
 		label: "Vimeo",
 		handle: "visioart",
 	},
